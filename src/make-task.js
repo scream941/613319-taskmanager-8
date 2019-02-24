f5b2bcdab20ff4d499fafbb90abae849668a0fc0
scream941 <scream941@mail.ru>
2019-02-25 00:32:34 +0300

    добавил функцию для отрисовки однотипного html

--- src/make-task.js
+++ src/make-task.js
@@ -1,3 +1,57 @@
+const daysList = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
+const hashtagsList = [`repeat`, `cinema`, `entertaiment`];
+const colorsList = [`black`, `yellow`, `blue`, `green`, `pink`];
+
+const dayCheckbox = (caption) => `
+  <input
+    class="visually-hidden card__repeat-day-input"
+    type="checkbox"
+    id="repeat-${caption}-5"
+    name="repeat"
+    value="${caption}"
+  />
+  <label class="card__repeat-day" for="repeat-${caption}-5"
+    >${caption}</label
+  >
+  `;
+
+const hashtag = (caption) => `<span class="card__hashtag-inner">
+  <input
+    type="hidden"
+    name="hashtag"
+    value="${caption}"
+    class="card__hashtag-hidden-input"
+  />
+  <button type="button" class="card__hashtag-name">
+    #${caption}
+  </button>
+  <button type="button" class="card__hashtag-delete">
+    delete
+  </button>
+</span>`;
+
+const color = (caption) =>
+  `<input
+    type="radio"
+    id="color-${caption}-5"
+    class="card__color-input card__color-input--${caption} visually-hidden"
+    name="color"
+    value="${caption}"
+  />
+  <label
+    for="color-${caption}-5"
+    class="card__color card__color--${caption}"
+    >${caption}</label
+  >`;
+
+const makeHtml = (array, func) => {
+  let a = ``;
+  for (let i = 0; i < array.length; i++) {
+    a += func(`${array[i]}`);
+  }
+  return a;
+};
+
 export default () => `<article class="card card--blue">
   <form class="card__form" method="get">
     <div class="card__inner">
@@ -64,130 +118,14 @@ export default () => `<article class="card card--blue">
 
             <fieldset class="card__repeat-days" disabled>
               <div class="card__repeat-days-inner">
-                <input
-                  class="visually-hidden card__repeat-day-input"
-                  type="checkbox"
-                  id="repeat-mo-5"
-                  name="repeat"
-                  value="mo"
-                  checked
-                />
-                <label class="card__repeat-day" for="repeat-mo-5"
-                  >mo</label
-                >
-                <input
-                  class="visually-hidden card__repeat-day-input"
-                  type="checkbox"
-                  id="repeat-tu-5"
-                  name="repeat"
-                  value="tu"
-                  checked
-                />
-                <label class="card__repeat-day" for="repeat-tu-5"
-                  >tu</label
-                >
-                <input
-                  class="visually-hidden card__repeat-day-input"
-                  type="checkbox"
-                  id="repeat-we-5"
-                  name="repeat"
-                  value="we"
-                />
-                <label class="card__repeat-day" for="repeat-we-5"
-                  >we</label
-                >
-                <input
-                  class="visually-hidden card__repeat-day-input"
-                  type="checkbox"
-                  id="repeat-th-5"
-                  name="repeat"
-                  value="th"
-                />
-                <label class="card__repeat-day" for="repeat-th-5"
-                  >th</label
-                >
-                <input
-                  class="visually-hidden card__repeat-day-input"
-                  type="checkbox"
-                  id="repeat-fr-5"
-                  name="repeat"
-                  value="fr"
-                  checked
-                />
-                <label class="card__repeat-day" for="repeat-fr-5"
-                  >fr</label
-                >
-                <input
-                  class="visually-hidden card__repeat-day-input"
-                  type="checkbox"
-                  name="repeat"
-                  value="sa"
-                  id="repeat-sa-5"
-                />
-                <label class="card__repeat-day" for="repeat-sa-5"
-                  >sa</label
-                >
-                <input
-                  class="visually-hidden card__repeat-day-input"
-                  type="checkbox"
-                  id="repeat-su-5"
-                  name="repeat"
-                  value="su"
-                  checked
-                />
-                <label class="card__repeat-day" for="repeat-su-5"
-                  >su</label
-                >
+              ${makeHtml(daysList, dayCheckbox)}
               </div>
             </fieldset>
           </div>
 
           <div class="card__hashtag">
             <div class="card__hashtag-list">
-              <span class="card__hashtag-inner">
-                <input
-                  type="hidden"
-                  name="hashtag"
-                  value="repeat"
-                  class="card__hashtag-hidden-input"
-                />
-                <button type="button" class="card__hashtag-name">
-                  #repeat
-                </button>
-                <button type="button" class="card__hashtag-delete">
-                  delete
-                </button>
-              </span>
-
-              <span class="card__hashtag-inner">
-                <input
-                  type="hidden"
-                  name="hashtag"
-                  value="repeat"
-                  class="card__hashtag-hidden-input"
-                />
-                <button type="button" class="card__hashtag-name">
-                  #cinema
-                </button>
-                <button type="button" class="card__hashtag-delete">
-                  delete
-                </button>
-              </span>
-
-              <span class="card__hashtag-inner">
-                <input
-                  type="hidden"
-                  name="hashtag"
-                  value="repeat"
-                  class="card__hashtag-hidden-input"
-                />
-                <button type="button" class="card__hashtag-name">
-                  #entertaiment
-                </button>
-                <button type="button" class="card__hashtag-delete">
-                  delete
-                </button>
-              </span>
+              ${makeHtml(hashtagsList, hashtag)}
             </div>
 
             <label>
@@ -212,67 +150,7 @@ export default () => `<article class="card card--blue">
         <div class="card__colors-inner">
           <h3 class="card__colors-title">Color</h3>
           <div class="card__colors-wrap">
-            <input
-              type="radio"
-              id="color-black-5"
-              class="card__color-input card__color-input--black visually-hidden"
-              name="color"
-              value="black"
-            />
-            <label
-              for="color-black-5"
-              class="card__color card__color--black"
-              >black</label
-            >
-            <input
-              type="radio"
-              id="color-yellow-5"
-              class="card__color-input card__color-input--yellow visually-hidden"
-              name="color"
-              value="yellow"
-            />
-            <label
-              for="color-yellow-5"
-              class="card__color card__color--yellow"
-              >yellow</label
-            >
-            <input
-              type="radio"
-              id="color-blue-5"
-              class="card__color-input card__color-input--blue visually-hidden"
-              name="color"
-              value="blue"
-            />
-            <label
-              for="color-blue-5"
-              class="card__color card__color--blue"
-              >blue</label
-            >
-            <input
-              type="radio"
-              id="color-green-5"
-              class="card__color-input card__color-input--green visually-hidden"
-              name="color"
-              value="green"
-              checked
-            />
-            <label
-              for="color-green-5"
-              class="card__color card__color--green"
-              >green</label
-            >
-            <input
-              type="radio"
-              id="color-pink-5"
-              class="card__color-input card__color-input--pink visually-hidden"
-              name="color"
-              value="pink"
-            />
-            <label
-              for="color-pink-5"
-              class="card__color card__color--pink"
-              >pink</label
-            >
+            ${makeHtml(colorsList, color)}
           </div>
         </div>
       </div>
